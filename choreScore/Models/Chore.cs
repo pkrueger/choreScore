4983a9d441397202041c8506224c1db1c02d3280
pkrueger <sendthistopatrick@gmail.com>
2022-10-26 16:29:08 -0600

    Create new chore in repository layer

--- choreScore/Models/Chore.cs
+++ choreScore/Models/Chore.cs
@@ -4,18 +4,18 @@ namespace choreScore.Models;
 
 public class Chore
 {
-  public string Id { get; private set; }
-  public string Name { get; private set; }
-  public bool IsComplete { get; private set; }
-  public DayOfTheWeek Day { get; private set; }
-  public int Priority { get; private set; }
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public bool IsComplete { get; set; }
+  public DayOfTheWeek Day { get; set; }
+  public int Priority { get; set; }
 
-  public Chore(string name, DayOfTheWeek day, int priority)
-  {
-    Id = Guid.NewGuid().ToString();
-    Name = name;
-    IsComplete = false;
-    Day = day;
-    Priority = priority;
-  }
+  // public Chore(string name, DayOfTheWeek day, int priority)
+  // {
+  //   Id = Guid.NewGuid().ToString();
+  //   Name = name;
+  //   IsComplete = false;
+  //   Day = day;
+  //   Priority = priority;
+  // }
 }
--- choreScore/Repositories/ChoresRepository.cs
+++ choreScore/Repositories/ChoresRepository.cs
@@ -14,12 +14,24 @@ public class ChoresRepository
   /// </summary>
   public List<Chore> Get(string creatorId)
   {
-    var sql = "SELECT * FROM chores WHERE creatorId = @creatorId";
+    var sql = "SELECT * FROM chores WHERE creatorId = @creatorId;";
     return _db.Query<Chore>(sql, new { creatorId }).ToList();
   }
+
   public Chore Get(int id)
   {
-    var sql = "SELECT * FROM chores WHERE id = @id";
+    var sql = "SELECT * FROM chores WHERE id = @id;";
     return _db.QueryFirstOrDefault<Chore>(sql, new { id });
+  }
+
+  public Chore Create(Chore choreData)
+  {
+    var sql = @"
+      INSERT INTO chores (name, day, priority) 
+      VALUES (@Name, @Day, @Priority);
+      SELECT LAST_INSERT_ID();
+    ";
+    choreData.Id = _db.ExecuteScalar<int>(sql, choreData);
+    return choreData;
   }
 }
