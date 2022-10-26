146dddab7ff510e49032b4c977f2a2680320bc07
pkrueger <sendthistopatrick@gmail.com>
2022-10-26 14:55:43 -0600

    Create table for chores

--- choreScore/sql/Chores.sql
+++ choreScore/sql/Chores.sql
@@ -1,3 +1,15 @@
 -- Active: 1666715598134@@SG-kiwi-myrtle-9554-6832-mysql-master.servers.mongodirector.com@3306@stuff
 
-CREATE TABLE IF NOT EXISTS chores 
+CREATE TABLE
+    IF NOT EXISTS chores (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        name VARCHAR(255) NOT NULL,
+        isComplete BOOLEAN NOT NULL,
+        day VARCHAR(255) NOT NULL,
+        priority INT NOT NULL CHECK(
+            priority > 0
+            AND priority < 5
+        )
+    )
