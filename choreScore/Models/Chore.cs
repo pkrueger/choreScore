6dfbc9cbdfb1d0b4915394ac7c15beddb0176ace
pkrueger <sendthistopatrick@gmail.com>
2022-10-26 14:22:39 -0600

    Update gitignore and chore model

--- choreScore/Models/Chore.cs
+++ choreScore/Models/Chore.cs
@@ -8,13 +8,15 @@ public class Chore
   public string Name { get; private set; }
   public bool IsComplete { get; private set; }
   public DayOfTheWeek Day { get; private set; }
+  public int Priority { get; private set; }
 
-  public Chore(string name, DayOfTheWeek day)
+  public Chore(string name, DayOfTheWeek day, int priority)
   {
     Id = Guid.NewGuid().ToString();
     Name = name;
     IsComplete = false;
     Day = day;
+    Priority = priority;
   }
 
 }
